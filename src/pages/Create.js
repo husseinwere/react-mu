f9044a707e96a66ddec48e5cf0af6a564c260f12
Hussein Were <hussein.were@yahoo.com>
2024-03-20 10:25:19 +0300

    material ui

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,9 +1,20 @@
 import React from 'react'
+import Typography from '@mui/material/Typography';
 
 export default function Create() {
   return (
-    <div>
-      Create page
-    </div>
+    // <Typography
+    //   variant='h1'
+    //   color='primary'
+    //   align='center'
+    // >
+    //   Create a new note
+    // </Typography>
+    <Typography
+      color='secondary'
+      noWrap
+  >
+    balsdkfa sdf ajsdlf asdfjas df sdfj asdf jsaldk fjaslkd fs df asjdf lasdjfk lsda fa sdfjlasdf asdl fjaslkdf jaskd falskd flksad fa lsdf
+  </Typography>
   )
 }
